8da12c1fe81e4d771fd731e0c50ad35a272ae82e
Memerax <josephwhite@gmx.com>
2023-03-22 18:19:38 -0230

    created db.py and moved reader and writer functions to it , as per the assignment

---
+++ db.py
@@ -0,0 +1,16 @@
+
+def read_money_from_file():
+    try:
+        with open("money.txt", 'r') as file:
+            money = int(file.read())
+    except FileNotFoundError:
+        print("File not found")
+    return money
+
+
+def write_money_to_file(money):
+    try:
+        with open("money.txt", 'w') as file:
+            file.write(str(money))
+    except FileNotFoundError:
+        print("File not found")
