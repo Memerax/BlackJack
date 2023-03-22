9968fe8d6c278c30b0c50818e75278a15d37e55a
Memerax <josephwhite@gmx.com>
2023-03-22 17:22:01 -0230

    Made function that builds the deck, and a print deck function for testing

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+    pass
+
+
+def create_deck():
+    # Could have just created them all by hand, but I try to stick
+    # to the do not repeat yourself concept where possible
+    cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+             "10", "Jack", "Queen", "King"]
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    deck = []
+    for card in cards:
+        for suit in suits:
+            if card.isdigit():
+                deck.append([card, suit, int(card)])
+            elif card == "Ace":
+                deck.append([card, suit, 1, 11])
+            else:
+                deck.append([card, suit, 10])
+    return deck
+
+
+def print_deck(deck):
+    deck = create_deck()
+    for i in deck:
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
